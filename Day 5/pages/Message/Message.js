34d2be684175798707d7478614e087b834496170
Prasath s <110531926+Prasath16022004@users.noreply.github.com>
2023-09-21 10:47:53 +0530

    Add files via upload
    Review-2

---
+++ Day 5/pages/Message/Message.js
@@ -0,0 +1,17 @@
+//Author: Jainil Sevalia(jn498899@dal.ca) || Banner Id: B00925445
+
+import React from "react";
+import "./Message.css";
+
+const Message = () => {
+  return (
+    <div>
+      <h1>Name:Prasath</h1>
+      <h1>Phn No:7695829323</h1>
+      <h1>email:prasath76@gmail.com</h1>
+
+    </div>
+  );
+};
+
+export default Message;
