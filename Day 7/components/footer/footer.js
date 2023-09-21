5f96fd5cb1cf7db4bc29759c9a48471db3a58b0a
Prasath s <110531926+Prasath16022004@users.noreply.github.com>
2023-09-21 10:50:48 +0530

    Add files via upload
    Review

---
+++ Day 7/components/footer/footer.js
@@ -0,0 +1,48 @@
+//  import React from 'react'
+// import "../footer/footer.scss";
+
+// // export default function Footer() {
+// //   return (
+// //     <div></div>
+// //   )
+// // }
+
+
+// export default function Footer() {
+//     return (
+//       <div id="footer container">
+  
+  
+//       <div id="footer-section">
+//       <p id="title">TravvelExpenses.com</p>
+//       <p>TravelExpenses Tracker is a place where you can please your soul and tummy with delicious food recepies
+//       of all cuisine.And our service is absolutely free.</p>
+//       <p>&copy; @2022 | All Rights Reserved</p>
+//       </div>
+  
+      
+//       <div id="footer-section">
+//       <p id="title">Contact Us</p>
+//       <p>travelexpenses@gmail.com</p>
+//       <p>+91 7695829323</p>
+//       <p>2393 street NYC</p>
+     
+//       </div>
+      
+      
+//       <div id="footer-section">
+//       <p id="title">Socials</p>
+//       <p>Facebook</p>
+//       <p>Twitter</p>
+     
+//       <p>Instagram</p>
+     
+     
+//       </div>
+      
+      
+  
+      
+//       </div>
+//     )
+//   }
